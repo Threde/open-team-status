3aa12087870c1633d4fc6ccb3684fd05ef8936d6
Daniel Schep <dschep@gmail.com>
2016-07-30 11:46:49 -0400

    more details in magiclink admin

--- magiclink/admin.py
+++ magiclink/admin.py
@@ -5,5 +5,6 @@ from .models import MagicToken
 
 @admin.register(MagicToken)
 class MagicTokendmin(admin.ModelAdmin):
-        list_display = ('user', 'ttl', 'magictoken')
-        readonly_fields = ('__str__',)
+        list_display = ('user', 'created', 'ttl', '__str__')
+        list_filter = ('created', 'user')
+        readonly_fields = ('created', '__str__')

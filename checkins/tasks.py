7feff32e1db42a474efe7f768c471a9103a65b38
Daniel Schep <dschep@gmail.com>
2016-07-30 10:48:04 -0400

    make reminder link redirect to reminder

--- checkins/tasks.py
+++ checkins/tasks.py
@@ -1,6 +1,7 @@
 from celery import shared_task
 from django.conf import settings
 from django.core.mail import send_mail
+from django.core.urlresolvers import reverse
 from django.contrib.auth import get_user_model
 
 from magiclink.models import MagicToken
@@ -9,11 +10,16 @@ from magiclink.models import MagicToken
 import requests
 
 
+def make_magic_checkin_link(user):
+	link = MagicToken(user=user)
+	link.save()
+	return '{}?next={}'.format(link, reverse('checkin-create')
+
+
 @shared_task
 def slack_reminder():
     for user in get_user_model().objects.all():
-        link = MagicToken(user=user)
-        link.save()
+        link = make_magic_checkin_link(user)
         requests.post(settings.OPEN_TEAM_STATUS_REMINDER_SLACK_WEBHOOK,
                       json={
                           'channel': '@' + user.username,
@@ -29,8 +35,7 @@ def email_reminder():
     for user in get_user_model().objects.all():
         if not user.email:
             continue
-        link = MagicToken(user=user)
-        link.save()
+        link = make_magic_checkin_link(user)
         send_mail(settings.OPEN_TEAM_STATUS_REMINDER_SUBJECT,
                   settings.OPEN_TEAM_STATUS_REMINDER_BODY.format(url=link),
                   settings.DEFAULT_FROM_EMAIL,
